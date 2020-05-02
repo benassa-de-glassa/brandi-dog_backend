4d7dc544a32337ee76eac34274062070261ce46b
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-05-02 11:57:41 +0200

    Redesign of field and marble class
    The field is now a double linked list instead of a standard python
    list. This facilitates marble movement along the field as well as
    exiting the playing field to reach a players goal.
    
    The marble object is now made to fit the field object and takes
    care of remembering its position. It further remembers whether it
    is allowed to enter the goal and wheteher it is blocking other
    marbles in the starting position.

--- app/game_logic/field.py
+++ app/game_logic/field.py
@@ -1,9 +1,83 @@
+NODES_BETWEEN_PLAYERS = 16
+
+class Node(object):
+    def __init__(self):
+        self.next = None
+        self.prev = None
+    
+class GameNode(Node):
+    def __init__(self, position):
+        self.position = None
+        self.marble = None
+
+    def set_marble(self, marble):
+        self.marble = marble
+
+    def unset_marble(self):
+        self.marble = None
+
+    def is_blocking(self):
+        """
+        
+        A game node cannot be blocking 
+        This is a convinience function
+        """
+        return False 
+    def has_marble(self):
+        return self.marble is not None
+
+class EntryExitNode(GameNode):
+    def __init__(self, uid, position):
+        super().__init__(self, position)
+
+        self.entry_exit_for_player = uid
+        self.exit = [None, None, None ,None]
+        
+
+    def is_blocking(self):
+        """
+        returns true if a marble is placed in this node and the marble is blocking
+        returns false else
+
+        """
+        if self.marble is not None:
+            return self.marble.blocking
+        
+        return False
+
+
 
 class Field():
     """
-    Field Object
-
+    Field is a doubly linked list with additional entry nodes at the players starting postions
     """
+    def __init__(self, players: list):
+        """
+        
+        players: list of player uids
+        """    
+        self.entry_nodes = {} # store the entry nodes for each of the players
+        
+        nodes = []
 
-    def __init__(self):
-        self.marbles = [0] * 4 * 16
+
+        for i in range(len(players)): # range(4)
+            new_entry_node = EntryExitNode(players[i], len(nodes))
+            new_entry_node.entry_exit_for_player = players[i]
+            self.entry_nodes[players[i]] = new_entry_node
+            nodes.append(new_entry_node)
+            
+
+            for _ in range(NODES_BETWEEN_PLAYERS - 1):
+                new_node = GameNode(len(nodes))
+                nodes.append(new_node)
+            
+        # connect the nodes
+        for index in range(len(nodes)):
+            nodes[index].next = nodes[(index + 1 ) % len(nodes)]
+            nodes[index].prev = nodes[(index - 1 ) % len(nodes)]
+
+        self.game_start_node = nodes[0]
+        
+    def get_starting_node(self, player):
+        return self.entry_nodes[player.uid]
--- app/game_logic/marble.py
+++ app/game_logic/marble.py
@@ -6,6 +6,44 @@ class Marble():
     
     """
 
-    def __init__(self, color):
-        self.color = color
+    def __init__(self, color, pid, starting_node):
+        self.prev = None
+        self.curr = None # start in the starting area
+        self.next = starting_node
 
+        # store the starting position separately for a reset
+        self.starting_position = starting_node
+
+        self.color = color
+        self.pid = pid # player id
+        self.blocking = False
+        self.can_enter_goal = False
+
+    def reset_to_starting_position(self):
+        self.curr.marble = None
+
+        self.prev = None
+        self.curr = None
+        self.next = self.starting_position
+
+        self.blocking = False
+        self.can_enter_goal = False
+
+    def set_new_position(self, node):
+        self.curr = node.curr
+        self.prev = node.prev
+        self.next = node.next
+
+        node.marble = self
+
+    def to_json(self):
+        if self.curr is None: # at start
+            position = -1
+        else: 
+            position = self.curr.position
+
+
+        return {
+            'position': position,
+            'color': self.color
+        }
