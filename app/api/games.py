2140b55bad8f706193b9cd94a568675bb34cadcb
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-05-09 14:59:07 +0200

    Multiple changes to /games  route   - Added Socket events - untested
      - Added route POST /games/{game_id}/swap to allow a swap of cards
      - Added GET /games/{game_id}/cards route to get own cards
      - Refactored GET and POST /games

--- app/api/games.py
+++ app/api/games.py
@@ -6,16 +6,16 @@ from pydantic import BaseModel
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST
 
-from typing import List
+from typing import List, Optional
 
 # Brandi game object
 from app.game_logic.brandi import Brandi
 
 # models
-from app.models.player import Player
+from app.models.player import Player, PlayerPublic, PlayerPrivate
 from app.models.action import Action
-from app.models.card import Card
-from app.models.game import GameList
+from app.models.card import Card, CardBase
+from app.models.game import GamePublic
 
 router = APIRouter()
 
@@ -23,21 +23,42 @@ games = {}
 
 from app.main import sio
 
+"""
+socket events
+"""
+def sio_emit_game_state(game_id):
+    sio.emit(
+        'game-state',
+        {
+            "data": games[game_id].public_state()
+        },
+        room=game_id
+    )
 
-@router.get('/games', response_model=GameList)
+@sio.on('join-game')
+def join_websocket(sid, data):
+    if data["game_id"] not in games:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="The game you attempt to join does not exist.")
+    sio.enter_room(sid, data["game_id"])
+    sio.emit('success', 
+        {
+            "response": f"successfully joined game {data['game_id']}"
+        }, 
+        room=sid)
+
+
+"""
+routing
+"""
+@router.get('/games', response_model=List[GamePublic])
 def get_list_of_games():
     """
     return a list of game keys
     """
-    return [
-        {
-            'game_id' : game_key,
-            'players' : [player.to_json() for player in game_instance.players.values()]
-        } 
-        for game_key, game_instance in games.items()]
+    return [game_instance.public_state() for game_instance in games.values()]
 
-@router.post('/games', response_model=GameList)
-def initialize_new_game(player: Player):
+@router.post('/games', response_model=GamePublic)
+def initialize_new_game( player: Player, seed: int=None):
     """
     start a new game
     """
@@ -46,19 +67,11 @@ def initialize_new_game(player: Player):
     while game_id in games:
         game_id = ''.join(random.choice(string.ascii_uppercase) for i in range(4)) # generate new game ids until a new id is found
 
-    games[game_id] = Brandi()
+    games[game_id] = Brandi(game_id, seed=seed)
     games[game_id].player_join(player)
-    return {
-        'games': [
-            {
-                'game_id' : game_key,
-                'players' : [player.to_json() for player in game_instance.players.values()]
-            } 
-            for game_key, game_instance in games.items()
-        ]
-    }
+    return games[game_id].public_state()
 
-@router.get('/games/{game_id}')
+@router.get('/games/{game_id}', response_model=GamePublic)
 def get_game_state(game_id: str, player: Player):
     """
     get the state of a game
@@ -67,36 +80,35 @@ def get_game_state(game_id: str, player: Player):
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST, detail="Player not in Game."
         )
+
     return games[game_id].public_state()
 
-@router.post('/games/{game_id}/join')
+@router.post('/games/{game_id}/join', response_model=GamePublic)
 def join_game(game_id: str, player: Player):
     """
     join an existing game
     """
+    if game_id not in games: # ensure the game exists
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Game with game id {game_id} does not exist.")
     if player.uid in games[game_id].players: # ensure no player joins twice
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.name} has already joined.")
-        
+    if len(games[game_id].players) ==4:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Four player have already joined this game, there is no more room.")
+
     games[game_id].player_join(player)
     return games[game_id].public_state()
 
-@router.post('/games/{game_id}/teams')
+@router.post('/games/{game_id}/teams', response_model=GamePublic)
 def set_teams(game_id: str, player: Player, teams: List[Player]):
     if player.uid not in games[game_id].players :
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.uid} not in Game.")
     if not all([_p.uid in games[game_id].players for _p in teams]): # check validity of teams
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Players not in Game.")
         
-    if not len(set(teams)) == len(teams): # assert no doule players
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Not all players selected in request.")
+    # if not len(frozenset(teams)) == len(teams): # assert no doule players
+    #     raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Not all players selected in request.")
 
-    games[game_id].change_teams([player.uid for player in teams])
-    return games[game_id].public_state()
-
-@router.post('/games/{game_id}/ready', response_model=Player)
-def toggle_player_ready(game_id: str, player: Player):
-    if player.uid not in games[game_id].players :
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.uid} not in Game.")
+    games[game_id].change_teams(teams)
     return games[game_id].public_state()
 
 @router.post('/games/{game_id}/start')
@@ -111,14 +123,43 @@ def start_game(game_id: str, player: Player):
     games[game_id].start_game()
     return games[game_id].public_state()
 
-@router.post('/games/{game_id}/swap')
-def swap_card(game_id: str, player: Player, card: Card):
-    pass
+@router.get('/games/{game_id}/cards')
+def start_game(game_id: str, player: Player):
+    """
+    start an existing game
+    """
+    if game_id not in games: # ensure the game exists
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Game with game id {game_id} does not exist.")
+    if player.uid not in games[game_id].players:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.uid} not in Game.")
+    return games[game_id].get_cards(player)
+    
 
+@router.post('/games/{game_id}/swap_cards', response_model=PlayerPrivate)
+def swap_card(game_id: str, player: Player, card: CardBase):
+    """
+    make the card swap before starting the round
+    """
+    if player.uid not in games[game_id].players:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.uid} not in Game.")
+
+    if card.uid not in games[game_id].players[player.uid].hand.cards:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Card {card.uid} not in {player.name}'s hand.")
+
+    return games[game_id].swap_card(player, card)
+    
 @router.post('/games/{game_id}/action')
 def perform_action(game_id: str, player: Player, action: Action):
     """
     
     """
+
+    if player.uid not in games[game_id].players:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Player {player.uid} not in Game.")
+
+    if action.card.uid not in games[game_id].players[player.uid].hand.cards:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=f"Card {action.card.uid} not in {player.name}'s hand.")
+
+
     games[game_id].event_play_card(player, action)
     return games[game_id].public_state()
