43d9355698c2a59fe19b971b8c544061975da066
thilo <thiloschmid@bluewin.ch>
2020-05-10 19:44:20 +0200

    added simple chat functionality

--- app/api/chats.py
+++ app/api/chats.py
@@ -1,9 +1,20 @@
-
+import datetime
 from fastapi import APIRouter, Path
 
 from pydantic import BaseModel
+
+from app.api.socket import sio
 
 router = APIRouter()
 
 class Message(BaseModel):
     pass
+
+
+@sio.event
+async def chat_message(sid, message):
+    # message should be a {'sender': str, 'text': str} dictionary
+
+    #add time to message
+    message['time'] = datetime.datetime.now().strftime('%H:%M:%S')
+    await sio.emit('chat_message', message)
