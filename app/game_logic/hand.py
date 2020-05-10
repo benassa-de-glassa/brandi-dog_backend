7a896f7353750f7784caf7b9a5526d7c4c3c2a0c
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-05-10 18:25:10 +0200

    reset hand with fold() function

--- app/game_logic/hand.py
+++ app/game_logic/hand.py
@@ -10,6 +10,9 @@ class Hand():
         assert card.uid not in self.cards
         self.cards[card.uid] = card
 
+    def fold(self):
+        self.cards = {}
+
     def to_json(self):
 
         return [card.to_json() for card in self.cards.values()]
