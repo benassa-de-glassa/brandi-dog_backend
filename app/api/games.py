e81597a6d4b2f13a3d51ae31985d25e43988e9b4
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-05-24 20:17:44 +0200

    emit the gamestate after all cards have been swapepd

--- app/api/games.py
+++ app/api/games.py
@@ -199,6 +199,8 @@ async def swap_card(game_id: str, player: Player, card: CardBase):
     if res["taskFinished"]:
         for uid in games[game_id].order:
             await sio_emit_player_state(game_id, uid)
+        await sio_emit_game_state(game_id)
+        
     return res  # do not return cards at this point as the player is not allowed to view them yet
 
 
