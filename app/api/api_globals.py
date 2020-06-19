69d41c2aa8f6576591445edb70104dedee4f24bc
thilo <thiloschmid@bluewin.ch>
2020-06-19 17:07:18 +0200

    chat messages in a game are now only sent to the game

--- app/api/api_globals.py
+++ app/api/api_globals.py
@@ -2,4 +2,5 @@
 Attempt to avoid circular imports by having globally used variables in one place only.
 """
 
+# {player_id: game_id}
 playing_users = {}
--- app/api/chats.py
+++ app/api/chats.py
@@ -8,14 +8,14 @@ from app.api.socket import sio
 router = APIRouter()
 
 
-class Message(BaseModel):
-    pass
-
-
 @sio.event
 async def chat_message(sid, message):
     # message should be a {'sender': str, 'text': str} dictionary
-
     # add time to message
     message['time'] = datetime.datetime.now().strftime('%H:%M:%S')
-    await sio.emit('chat_message', message)
+    await sio.emit('chat_message', message, room=message['game_id'])
+
+@sio.event
+async def global_chat_message(sid, message):
+    message['time'] = datetime.datetime.now().strftime('%H:%M:%S')
+    await sio.emit('global_chat_message', message)
